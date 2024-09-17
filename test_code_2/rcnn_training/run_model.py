26b639a92297eb37fcaf9dfc2cdaaae7ed63336e
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2024-09-17 09:26:43 -1000

    Removed unnecessary files.

--- test_code_2/rcnn_training/run_model.py
+++ test_code_2/rcnn_training/run_model.py
@@ -50,49 +50,6 @@ def visualize_predictions(image, outputs, threshold=0.8):
 
     plt.show()
 
-# TODO: check this out: alternate function that uses separate thresholds for separate classes
-# def visualize_predictions(image, outputs, class_thresholds, default_threshold=0.1):
-#     # Get bounding boxes, labels, and scores
-#     boxes = outputs[0]['boxes'].detach().cpu().numpy()
-#     labels = outputs[0]['labels'].detach().cpu().numpy()
-#     scores = outputs[0]['scores'].detach().cpu().numpy()
-
-#     fig, ax = plt.subplots(1)
-#     ax.imshow(image)
-
-#     # Draw bounding boxes
-#     for box, label, score in zip(boxes, labels, scores):
-#         # Get the threshold for the current class, or use the default if not specified
-#         threshold = class_thresholds.get(label, default_threshold)
-        
-#         if score >= threshold:  # Only show predictions above the threshold for their class
-#             xmin, ymin, xmax, ymax = box
-#             width, height = xmax - xmin, ymax - ymin
-#             rect = patches.Rectangle((xmin, ymin), width, height, linewidth=1, edgecolor='r', facecolor='none')
-#             ax.add_patch(rect)
-#             plt.text(xmin, ymin, f'{labels_dict[label]}: {score:.2f}', color='white', fontsize=8, bbox=dict(facecolor='none', edgecolor='none', pad=0))
-
-#     plt.show()
-
-# # Define class-specific thresholds
-# class_thresholds = {
-#     0: 0.1,  # Threshold for 'fish'
-#     1: 0.2,  # Threshold for 'bait_arm'
-#     2: 0.3   # Threshold for 'background'
-# }
-
-# # Load and preprocess the image
-# image_tensor, image = preprocess_image('test_code_2/rcnn_training/LeFish.png')
-
-# # Run the model on the image
-# with torch.no_grad():
-#     predictions = model(image_tensor)
-
-# # Visualize the predictions with class-specific thresholds
-# visualize_predictions(image, predictions, class_thresholds)
-
-
-
 # Load and preprocess the image
 image_tensor, image = preprocess_image('test_code_2/rcnn_training/LeFish.png')
 
@@ -100,7 +57,6 @@ image_tensor, image = preprocess_image('test_code_2/rcnn_training/LeFish.png')
 with torch.no_grad():
     predictions = model(image_tensor)
 
-print(predictions)
 # Visualize the predictions
 visualize_predictions(image, predictions)
 
