9a2862cf32f13ab90510b5475b8092968be10746
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2024-09-23 15:19:38 -1000

    Switched to pandas and a class model.

--- YOLO_test_code/MOUSS_style_inference.py
+++ YOLO_test_code/MOUSS_style_inference.py
@@ -14,72 +14,120 @@ from ultralytics import YOLO, solutions
 import os
 import time 
 import csv
+import pandas as pd 
 
-cwd = os.getcwd() # Fish_no_fish project root
 
-model_path = os.path.join(cwd, 'models/yolov8n_fish_trained_lgds.pt')
+class FishDetector:
+    def __init__ (self):
+        self.cwd = os.getcwd() # Fish_no_fish project root
 
-print("hello world")
+        ## Specify path to model
+        self.model_path = os.path.join(self.cwd, 'models/yolov8n_fish_trained_lgds.pt')
 
-model = YOLO(model_path)
+        self.model = YOLO(self.model_path)
 
-images_dir = os.path.join(cwd, 'test_code_2/rcnn_training/fish_data/fish_images')
+        ## Specify path to images
+        self.images_dir = os.path.join(self.cwd, 'test_code_2/rcnn_training/fish_data/fish_images')
 
-def create_csv(directory):
-    header = ['image', 'number of detections']
-    with open(directory, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(header)
+        self.wait_for_new_images_time = 4 # Seconds, before the program starts analyzing metrics
+        self.need_header = True
 
-def write_line(directory, image_name, num_detections):
-    line = [image_name, num_detections]
-    with open(directory, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(line)
+        self.analyze_every_x_frame = 1  # Analyze every (2nd, 3rd, 4th,... ect) frame
+                                        # This must be a tunable parameter
 
-def process_images(images_path):
-    count = 0 
-    processed_imgs = set() 
+        self.metrics_chunk_size = (6 * 60) / self.analyze_every_x_frame  # 6 frames per second * 60 frames in a minute / how many we actually analyze 
+                                        # batch size MUST represent 1 minute of data 
+                                        # for statistical accuracy. 
+                                        # Max = 360 (frames in one minute of data)
 
-    while(True): 
-        all_images = os.listdir(images_path)
-        num_images = len(all_images)
-
-        # Get all the unprocessed images
-        images = [cur_image for cur_image in all_images if cur_image.endswith(('.jpg', '.png')) and cur_image not in processed_imgs]
+        self.num_images = 0 
         
-        for image in images:
-            image_path = os.path.join(images_path, image)
+        print("image batch size = ", self.metrics_chunk_size)
+    def write_line(self, directory, image_name, num_detections):
+        '''
+        Write one line in the image/detection CSV.
+        Args: - (string) directory where csv was created [should be same as above]
+        - (string) name of image as it will appear in the csv
+        - (int) number of detections associated with current image
+        '''
+        df = pd.DataFrame([[image_name, num_detections]], columns=['Image', 'Num_detections'])
+        df.to_csv(directory, mode='a', header=self.need_header, index=False) 
+        self.need_header = False
 
-            # Run inference and store number of detections
-            inference = model(image_path)
+    def process_images(self, images_path):
+        '''
+        Run inference on every image in the current directory.
+        Wait ten seconds (change for MOUSS), and run inference on
+        any new images or exit. Add image name and num detections to
+        the CSV.
+        Args: - (string) path to image folder
+        '''
+        count = 0 
+        processed_imgs = set() 
 
-            for inference in inference:
-                boxes = inference.boxes
-                detection_count = len(boxes)
+        while(True): 
+            all_images = os.listdir(images_path)
+            self.num_images = len(all_images)
 
-            # meta_data.append([image, detection_count]) # write as you go, instead of at the end 
-            write_line(os.path.join(cwd, 'detections.csv'), image, detection_count)
+            # Get all the unprocessed images
+            images = [cur_image for cur_image in all_images if cur_image.endswith(('.jpg', '.png')) and cur_image not in processed_imgs]
             
+            for image in images:
+                image_path = os.path.join(images_path, image)
+
+                # Run inference and store number of detections
+                inference = self.model(image_path)
+
+                for inference in inference:
+                    boxes = inference.boxes
+                    detection_count = len(boxes)
+
+                # meta_data.append([image, detection_count]) # write as you go, instead of at the end 
+                self.write_line(os.path.join(self.cwd, 'detections.csv'), image, detection_count)
+                
 
 
-            processed_imgs.add(image)
-            print(count)
-            # print(processed_imgs)
-            count = count + 1 
+                processed_imgs.add(image)
+                print(count)
+                # print(processed_imgs)
+                count = count + 1 
 
-        # Wait a second and check if any new images were added
-        time.sleep(10)
-        all_images = os.listdir(images_path)
-        new_num_images = len(all_images)
-        if(new_num_images == num_images):
-            print("No new images, exiting...")
-            exit()
-        
+            # Wait a second and check if any new images were added
+            time.sleep(self.wait_for_new_images_time)
+            all_images = os.listdir(images_path)
+            new_num_images = len(all_images)
+            if(new_num_images == self.num_images):
+                print("No new images, exiting...")
+                break
 
-# main
-create_csv(os.path.join(cwd, 'detections.csv')) # Use current directory for now
-process_images(images_dir)
+    def get_metrics(self, path_to_csv):
+        chunk_size = self.metrics_chunk_size
+
+        for i in range(int(self.num_images / self.metrics_chunk_size) + 1): 
+            '''
+            Analyze all the data in chunks that equate to one minute of images 
+            '''
+            tmp_table = []
+            for df in pd.read_csv(path_to_csv, chunksize=chunk_size):
+                # tmp_table.append([df.Image, df.Num_detections])# TODO: this is coming off as two separate tables but should be one table. 
+                pass 
+            # print("temp table = ")
+            # print(tmp_table)
+            # print("table shape = ")
+            # print(len(tmp_table))
+
+def main():
+
+    fishDetector = FishDetector() 
+    # create_csv(os.path.join(cwd, 'detections.csv')) # Use current directory for now
+    fishDetector.process_images(fishDetector.images_dir)    # TODO: maybe move 
+    csv_path = os.path.join(fishDetector.cwd, 'detections.csv')
+    fishDetector.get_metrics(csv_path)
+
+
+
+if __name__ == '__main__': 
+    main()
 
 # Run inference on every image 
 # Store the image name and number of detections in csv file 
